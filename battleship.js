bc150865a799b62fe1a2cdaebfb6f2266e40d9ba
caprinevins <drewnevins@live.com>
2017-02-01 00:25:06 -0800

    added logic to alert user when they hit or missed the battleship, as well as give them their end game stats.

--- battleship.js
+++ battleship.js
@@ -24,11 +24,23 @@ while (isSunk == false) {
     guesses = guesses + 1;
     
     if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT!");
       hits = hits + 1;
+      
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sank the Battleship!");
+      }
+      else {
+        alert("You missed!");
+      }
     }
   }
 }
 
+var stats = "You took " + guesses + " guesses to sink the Battlship, " + "which means your shooting accuracy was " + (3 / guesses);
+
+alert(stats);
 
 /*pseudo code for the game:
 
